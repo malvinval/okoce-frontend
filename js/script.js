f773724ea197de221396ecbd08ad2b621ffe5d32
malvinval <malvinval09@gmail.com>
2023-09-22 14:32:31 +0700

    add: navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("ok");
+// select button and navbar element
+const toggleButton = document.getElementById("toggleButton");
+const navbar = document.getElementById("navbar-default");
+
+// add event listener to the button
+toggleButton.addEventListener("click", () => {
+    // toggle the 'hidden' class to show/hide the navbar
+    navbar.classList.toggle("hidden");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "jost": ['Jost', 'sans-serif'],
+      },
+      fontWeight: {
+        "bold-400": "400",
+        "bold-500": "500",
+        "bold-900": "900",
+        "bold-600": "600",
+        "bold-700": "700"
+      },
+      borderRadius: {
+        "half": "50px"
+      },
+      colors: {
+        "okoce-light-blue": "#3EB7DD",
+        "okoce-blue": "#007CBD",
+      }
+    },
   },
   plugins: [],
 }
