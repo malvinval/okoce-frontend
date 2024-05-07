112ca26e235c2f0e3304609f819f4d5b4dd4a6ea
Hagi <Hagiazzamaz@gmail.com>
2024-05-07 18:38:35 +0700

    add backgrounds to brand-lokal page

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,7 @@ module.exports = {
         "bg2" : "url('./img/bg2.png')",
         "bg3" : "url('./img/bg3.jpg')",
         "bg4" : "url('./img/banner-home.jpg')",
+        "bg5" : "url('./img/galeri/sandiaga.jpeg')",
         "pelatihan-ukm": "url('../7top-detail-pages/img/pelatihan-kolaborasi-ukm-strategi.jpg')",
         "pelatihan-master-mentor": "url('../7top-detail-pages/img/master-mentor-2.webp')",
         "pelatihan-optimasi-bisnis": "url('../7top-detail-pages/img/optimasi-bisnis.webp')",
